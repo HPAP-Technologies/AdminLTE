a87002b610b229a1690715e279f35e7d1c2e1cee
Glenn <1964713+pyromanci@users.noreply.github.com>
2024-08-03 13:58:53 -0400

    feat: favicon

--- src/config/assets.config.mjs
+++ src/config/assets.config.mjs
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 
 try {
   fs.copySync('./src/assets', './dist/assets')
+  fs.copySync('./src/favicon', './dist/pages')
   console.log('Assets copy success!')
 } catch (error) {
   console.error(error)
--- src/config/astro.config.mjs
+++ src/config/astro.config.mjs
@@ -14,6 +14,5 @@ export default defineConfig({
   },
   integrations: [mdx()],
   srcDir: './src/html',
-  cacheDir: './dist/pages',
   outDir: './dist/pages'
 })
